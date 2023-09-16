e87a856af86f2940ee5f6e923e7e5cb70f5b1d3c
huthbn <142831406+huthbn@users.noreply.github.com>
2023-09-16 15:57:23 -0400

    Add files via upload

---
+++ PowerShell/Lab_3.ps1
@@ -0,0 +1,28 @@
+﻿function getIP{ 
+
+    (get-netipaddress).ipv4address | Select-String "172*" 
+
+} 
+
+$IP = getIP
+
+#Claim the username variable to get the username
+
+$username = $env:USERNAME
+
+#Claim the variable to get the hostname of the device
+
+$hostname = $env:COMPUTERNAME
+
+#Powershell Version
+
+$powershellVersion = $PSVersionTable.PSVersion.Major
+
+#Get current date to show all but the time.
+
+$currentdate = Get-Date
+
+$date = "{0:MMMM d, yyyy}" -f $currentdate
+
+$BODY = "This machine's IP is $IP. User is $username. Hostname is $hostname. Powershell Version powershellVersion. Today's date is $date." 
+Write-Output "$BODY"

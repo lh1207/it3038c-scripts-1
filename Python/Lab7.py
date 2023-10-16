c7601d186dd9c2d79c2b4d8795f199ad5cd385af
huthbn <huthbn@mail.uc.edu>
2023-10-15 17:49:07 -0700

    Update Lab7.py

--- Python/Lab7.py
+++ Python/Lab7.py
@@ -1,3 +1,27 @@
 # This script will prompt the user to enter a string of characters and get Wikipedia results
 
-#Import 
+#Import the Wikipedia module
+
+import wikipedia
+
+# Ask user for input
+
+print("Hello! This prompt will retrieve information from Wikipedia on the content you search.")
+characters = input("What do you want to search for? " )
+
+#Print the search of keywords
+
+search = wikipedia.search(characters)
+print("The keywords for " + characters + ":" + str(search))
+
+#Print the summary of keywords
+
+summary = wikipedia.summary(characters)
+print("The summary for " + characters + ":" + str(summary))
+
+#Print the page of keywords
+
+wiki_page = wikipedia.page(characters).content
+
+print("The Wikipedia page for " + characters + ":" + str(wiki_page))
+print("I hope you enjoyed using the Wikipedia API!")

584fc5e29f921376301fa4bbedc97e126d682ebd
huthbn <huthbn@mail.uc.edu>
2023-10-15 20:17:15 -0400

    Update Lab7.py

--- Python/Lab7.py
+++ Python/Lab7.py
@@ -1,21 +1,3 @@
-#You will first ned to install the Wikipedia module before importing it
+# This script will prompt the user to enter a string of characters and get Wikipedia results
 
-import wikipedia
-
-# You can search for key words on Wikipedia by using wikipedia.search
-
-results = wikipedia.search('Cincinnati Bengals')
-
-print(results)
-
-# You can also find a summary of the keywords, which will produce a different output. You can also filter the output by sentences
-
-summary = wikipedia.summary("Cincinnati Bengals", sentences = 4)
-
-print(summary)
-
-# Finally, if you want to display the full contents of a Wikipedia page, you can do so with wikipedia.page
-
-wiki_page = wikipedia.page("Cincinnati Bengals").content
-
-print(wiki_page)  
+#Import 

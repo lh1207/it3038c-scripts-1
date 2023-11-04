be3d07771de05b55d8d93db278d46b22cd9e7f6a
huthbn <huthbn@mail.uc.edu>
2023-11-03 18:14:28 -0700

    Added api.js and weatherapi.py files

---
+++ Python/weatherapi.py
@@ -0,0 +1,10 @@
+import json 
+import requests 
+ 
+print('Please enter your zip code:') 
+zip = input() 
+ 
+r = requests.get('http://api.openweathermap.org/data/2.5/weather?zip=%s,us&appid=0fe3a2ab8c999bc1bd6d2ccc206edba1' % zip) 
+data=r.json() 
+print(data['weather'][0]['description'])
+
